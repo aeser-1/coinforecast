e01c4f009314abe3af083f0ecdc34096cde63122
Aktan Eser <60782694+aeser-1@users.noreply.github.com>
2021-07-04 21:08:23 +0300

    Cron Job Update

--- src/trade/tradecron.go
+++ src/trade/tradecron.go
@@ -16,7 +16,8 @@ func init() { //log initiation
 
 func main() {
 	cr := cron.New()
-
+	
+	//You should create a cron job for any strategy in tradealgo.
 	_, err := cr.AddFunc("25 0 */1 * * *", func() { //5 minute job
 		tradealgo.BTCAlgo()
 		log.Info("[Job 1]Trade algo BTC")
@@ -25,36 +26,12 @@ func main() {
 		panic(err.Error())
 	}
 
-	_, err1 := cr.AddFunc("30 0 */1 * * *", func() { //5 minute job
-		tradealgo.BCHAlgo()
-		log.Info("[Job 2]Trade algo BCH")
-	})
-	if err1 != nil {
-		panic(err1)
-	}
-
 	_, err2 := cr.AddFunc("35 0 */1 * * *", func() { //5 minute job
 		tradealgo.ETHAlgo()
 		log.Info("[Job 3]Trade algo ETH")
 	})
 	if err2 != nil {
 		panic(err2)
-	}
-
-	_, err3 := cr.AddFunc("40 0 */1 * * *", func() { //5 minute job
-		tradealgo.XRPAlgo()
-		log.Info("[Job 4]Trade algo XRP")
-	})
-	if err3 != nil {
-		panic(err3)
-	}
-
-	_, err4 := cr.AddFunc("45 0 */1 * * *", func() { //5 minute job
-		tradealgo.LTCAlgo()
-		log.Info("[Job 5]Trade algo LTC")
-	})
-	if err4 != nil {
-		panic(err4)
 	}
 
 	cr.Start()
